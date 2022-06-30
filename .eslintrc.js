e854d6dbfafc61ad01e1a2259400ec706e728d3c
Luiz Netto <luiz.netto@accenture.com>
2022-06-30 17:04:37 -0300

    Install Styled components lint lib and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:styled-components-a11y/recommended',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'styled-components-a11y'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -10,10 +13,9 @@ module.exports = {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
-
         '@typescript-eslint/no-explicit-any': 0,
         'react/static-property-placement': 0,
-
+        // prettier-ignore
         'quotes': [2, 'single'],
         'import/no-unresolved': 0,
         'comma-dangle': 0,
@@ -21,6 +23,7 @@ module.exports = {
         'react/state-in-constructor': 0,
         'no-underscore-dangle': 0,
         'react/jsx-props-no-spreading': 0,
+        // prettier-ignore
         'semi': 1,
         'comma-dangle:': 0,
         'import/prefer-default-export': 0,
